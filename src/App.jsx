64c39be5bf5d642df807974aa099d43d0ab8853e
0ldhands <iamkevinp11@gmail.com>
2025-04-01 08:56:52 -0700

    k

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { BrowserRouter as Router, Routes,Route} from "react-router-dom"
+import Homelayout from "./layout/Homelayout";
+import Aboutlayout from "./layout/Aboutlayout";
+import Contactlayout from "./layout/Contactlayout";
+import Rootlayout from "./layout/Rootlayout";
+import Detail from "./components/Detail";
+import Educations from "./components/Educations";
+import Skills from "./components/Skills";
+import Content from "./components/Content";
+import Projects from './components/Projects'
+import Certificate from './components/Certificate'
+import Hobbies from './components/Hobbies'
+import Internship from './components/Internship'
+import Achievements from './components/Achievements'
+import Experience from './components/Experience'
 
-function App() {
-  const [count, setCount] = useState(0)
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
+const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<Rootlayout />}>
+        <Route path="/" element={<Homelayout />}>
+          <Route path="/" element={<Content />}>
+            <Route path="" element={<Detail />} />
+            <Route path="Educations" element={<Educations />} />
+            <Route path="Skills" element={<Skills />} />
+            <Route path="Projects" element={<Projects />} />
+            <Route path="Certificate" element={<Certificate />} />
+            <Route path="Hobbies" element={<Hobbies />} />
+            <Route path="Internship" element={<Internship />} />
+            <Route path="Achievements" element={<Achievements />} />
+            <Route path="Experience" element={<Experience />} />
+          </Route>
+        </Route>
+        <Route path="About" element={<Aboutlayout />} />
+        <Route path="Contact" element={<Contactlayout />} />
+      </Route>
+    )
+  );
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      {/* <Router>
+        <Routes>
+          <Route path="/" element={<Homelayout/>}/>
+          <Route path="/About" element={<Aboutlayout/>}/>
+          <Route path="/Contact" element={<Contactlayout/>}/>
+        </Routes>
+      </Router> */}
+      <RouterProvider router={router} />
+    </div>
+  );
+};
 
-export default App
+export default App;
